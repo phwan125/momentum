c2e1ba61d273650404f6a581b266213af94b21a2
phwan125 <60490870+phwan125@users.noreply.github.com>
2021-08-08 10:24:28 +0900

    weather update

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function successFunction(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   console.log("you live in", latitude, longitude);
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
